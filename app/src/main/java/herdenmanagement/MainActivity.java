e36830f5c11ddbbe72b3233d63422806601da771
Lemgold <78437035+Lemgold@users.noreply.github.com>
2021-02-03 16:35:42 +0100

    Update MainActivity.java

--- app/src/main/java/herdenmanagement/MainActivity.java
+++ app/src/main/java/herdenmanagement/MainActivity.java
@@ -42,9 +42,41 @@ public class MainActivity extends AppCompatActivity
 
         initializeButtons();
 
+         mGestureDetector = new GestureDetectorCompat(this, new GestureListener());
 
 
+    }
+    
+    private class GestureListener extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
 
+            float abstandY = e2.getY() - e1.getY();
+            float abstandX = e2.getX() - e1.getX();
+
+            if(Math.abs(abstandX)>Math.abs(abstandY)){
+                if(abstandX>0){
+                    herdenManager.rind.bewegeRind(Rindvieh.RichtungsTyp.OST);
+                }else{
+                    herdenManager.rind.bewegeRind(Rindvieh.RichtungsTyp.WEST);
+                }
+            }else{
+                if(abstandY>0){
+                    herdenManager.rind.bewegeRind(Rindvieh.RichtungsTyp.SUED);
+                }else{
+                    herdenManager.rind.bewegeRind(Rindvieh.RichtungsTyp.NORD);
+                }
+            }
+            //etwas machen
+            //Toast.makeText(MainActivity.this, "Hallo",Toast.LENGTH_SHORT).show();
+            return super.onFling(e1, e2, velocityX, velocityY);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mGestureDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
     }
 
     protected void onStart()
