a78a3a8d0e21472e30405b869d011039e966dcb7
jarossg <58788016+jarossg@users.noreply.github.com>
2021-02-07 19:27:04 +0100

    Update GestureListener.java

--- app/src/main/java/herdenmanagement/model/GestureListener.java
+++ app/src/main/java/herdenmanagement/model/GestureListener.java
@@ -7,29 +7,36 @@ import herdenmanagement.HerdenManager;
 
 public class GestureListener extends GestureDetector.SimpleOnGestureListener {
     private HerdenManager herdenManager;
-
+    
+    //Konstruktor der den HerdenManager übernimmt um das Rind darauf zu bewegen
     public GestureListener(HerdenManager manager) {
         herdenManager = manager;
     }
 
+    //Hier wird das Wisch Event behandelt
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-
+        
+        //Mithilfe der Abstände wird die Richtung des Wischens ermittelt
         float abstandY = e2.getY() - e1.getY();
         float abstandX = e2.getX() - e1.getX();
-
+        
+        //Wenn die Distanz x größer als die von y ist wurde horizontal gewischt ansonsten vertikal
         if (Math.abs(abstandX) > Math.abs(abstandY)) {
+            //Wenn x größer als 0 ist wurde von links nach rechts gewischt ansonsten von rechts nach links
             if (abstandX > 0) {
                 herdenManager.getRind().bewegeRind(Rindvieh.RichtungsTyp.OST);
             } else {
                 herdenManager.getRind().bewegeRind(Rindvieh.RichtungsTyp.WEST);
             }
         } else {
+            //Wenn y größer als 0 ist wurde von oben nach unten gewischt ansonsten von unten nach oben
             if (abstandY > 0) {
                 herdenManager.getRind().bewegeRind(Rindvieh.RichtungsTyp.SUED);
             } else {
                 herdenManager.getRind().bewegeRind(Rindvieh.RichtungsTyp.NORD);
             }
+            //Die Bewegungsbefehle wurden entsprechend ausgeführt
         }
         //etwas machen
         //Toast.makeText(MainActivity.this, "Hallo",Toast.LENGTH_SHORT).show();
